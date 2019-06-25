ca4c41b7ec8c6ac198f44bb5995f2a524dbbd2d5
itoyu <yuuuuu0412@frontier.hokudai.ac.jp>
2019-06-25 12:10:17 +0900

    kadai10

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['prefix' => 'admin'], function(){
+  Route::get('news/create', 'Admin\NewsController@add');
+  Route::get('profile/create', 'Admin\ProfileController@add');
+  Route::get('profile/edit', 'Admin\ProfileController@edit');
+});
