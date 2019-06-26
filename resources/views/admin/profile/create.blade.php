55659881556fb735cf2b3a8fb59977405658badc
itoyu <yuuuuu0412@frontier.hokudai.ac.jp>
2019-06-26 17:54:35 +0900

    kadai12

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Profile</title>
-    </head>
-  <body>
-    <h1>edit.create</h1>
-  </body>
-</html>
+@extends('layouts.profile')
+@section('title', 'My プロフィール')
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col-md-8 mx-auto">
+      <h2>My プロフィール</h2>
+    </div>
+  </div>
+</div>
+@endsection
