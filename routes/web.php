2644b5b39be53b0f92497d5d2c61cd907e7993fd
itoyu <yuuuuu0412@frontier.hokudai.ac.jp>
2019-07-01 19:22:39 +0900

    kadai13

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin'], function(){
-  Route::get('news/create', 'Admin\NewsController@add');
-  Route::get('profile/create', 'Admin\ProfileController@add');
-  Route::get('profile/edit', 'Admin\ProfileController@edit');
+  Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+  Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+  Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
